88b4acf4fb1dc6b4d64119171bc650bcfd07b89c
Claudia dos Santos <aclaudia.santosd@gmail.com>
2026-05-31 16:19:35 +0200

    formadas las carpetas principales, junto con .env

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-import * as schema from "./schema";
-
-
-const sqlite = new Database("sqlite.db");
-
-//motor que usamos para el Route Handler de Gemini
-export const db = drizzle(sqlite, { schema });
